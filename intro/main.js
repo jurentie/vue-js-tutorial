c1ea01bcc84eaae49ed4a82d35d0bebe7bb25aee
jurentie <jurentie@gmail.com>
2019-02-21 16:43:55 -0700

    initial commit

---
+++ intro/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+	el: '#app',
+	data:{
+		product: 'Socks',
+		description: 'The finest socks in America!',
+		image: 'images/socks_green.png',
+		link: 'https://www.amazon.com',
+		inventory: 50,
+		onSale: false
+	}
+})
